3a5520c8a4bb7c1fbd3de74c9124f8e61a2c483f
Francis Marvin Rodriguez <francismarvinrodriguez@gmail.com>
2021-02-01 11:02:10 +0800

    fix validation date

--- app/src/main/java/govph/rsis/growapp/HomeActivity.java
+++ app/src/main/java/govph/rsis/growapp/HomeActivity.java
@@ -197,7 +197,7 @@ public class HomeActivity extends AppCompatActivity {
                                             progressDialog.show();
 
                                         if(isOnline()){
-                                            
+
                                             if(seedGrower.getVariety().matches("Select Variety") || seedGrower.getSeedsource().matches("Select Seed Source")
                                                     || seedGrower.getSeedclass().matches("Select Seed Class") || seedGrower.getDateplanted().matches("0002-11-30")
                                                     || seedGrower.getDateplanted().matches("-0001-11-30") || seedGrower.getDateplanted().matches("0000-00-00")
